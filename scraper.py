18f339dc4510ae550aed4b18920ae3c8eed73129
scrappycat <mchayat+scrappycatATgithub@gmail.com>
2015-07-30 07:53:12 +1000

    Added sold_on field

--- scraper.py
+++ scraper.py
@@ -3,23 +3,35 @@
 
 import scraperwiki
 from lxml import etree
-import datetime
+from datetime import datetime
 
 # Read in a page
-html = scraperwiki.scrape("http://house.ksou.cn/p.php?q=Caulfield+South%2C+VIC")
+suburb = "Caulfield+South"
+html = scraperwiki.scrape("http://house.ksou.cn/p.php?q=" + suburb +"%2C+VIC")
+
+# Drop previous data table
+try:
+    scraperwiki.sql.execute("DROP TABLE data")
+except:
+    print "table data not found"
 
 # Find something on the page using css selectors
 root = etree.HTML(html)
-extractedOn = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+
+# The scraper part where things break
 for elem in root.xpath("//tr[td/span[@class='addr']]/../../../.."):
     blob = etree.tostring(elem, pretty_print=True)
     address = elem.xpath(".//span[@class='addr']/a/text()")[0]
     sold = elem.xpath(".//span[@class='addr']/../../..//table/tr[1]/td/b/text()")[0]
-#    print sold.split()[1]
+    sold_on = elem.xpath(".//span[@class='addr']/../../..//table/tr[1]/td/text()")[0]
+    print sold_on
+    # Save found data
     scraperwiki.sqlite.save(unique_keys=['extracted_on','address'], data={
         "extracted_on": extractedOn,
         "address": address,
         "sold": sold.split()[1],
+        "sold_on": sold_on,
         "blob": blob
     })
 
@@ -27,7 +39,7 @@ for elem in root.xpath("//tr[td/span[@class='addr']]/../../../.."):
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 
 # An arbitrary query against the database
-print scraperwiki.sql.select("* from data")
+#print scraperwiki.sql.select("* from data")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
