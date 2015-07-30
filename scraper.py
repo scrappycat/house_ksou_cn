4ba5f45bd98525a478299b2c6b19bc0e0ad3dc48
scrappycat <mchayat+scrappycatATgithub@gmail.com>
2015-07-30 10:34:53 +1000

    Changed the main parsing structure to dictionary

--- scraper.py
+++ scraper.py
@@ -19,27 +19,72 @@ except:
 root = etree.HTML(html)
 extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 
+# translates row identifier and returns a dictionary with coolection of parsed parameters
+# to be merged with the original information dictionary
+def translate(value):
+    key = "".join(value[0].xpath("./b/text()")).split(":")[0].lower()
+    val = "".join(value[0].xpath("./text()"))
+    if key == "house" or key == "unit" or key == "townhouse":
+        (bedrooms, bathrooms) = val.split()
+        return {
+            "type": key,
+            "bedrooms": bedrooms,
+            "bathrooms": bathrooms
+        }
+
+    if key[0:4] == "sold":
+        return {
+            "sold": key.split()[1],
+            "sold on": val.strip()
+        }
+
+    return { key: val }
+
 # The scraper part where things break
 for elem in root.xpath("//tr[td/span[@class='addr']]/../../../.."):
     blob = etree.tostring(elem, pretty_print=True)
-    address = elem.xpath(".//span[@class='addr']/a/text()")[0]
-    sold = elem.xpath(".//span[@class='addr']/../../..//table/tr[1]/td/b/text()")[0]
-    sold_on = elem.xpath(".//span[@class='addr']/../../..//table/tr[1]/td/text()")[0]
-    print sold_on
+    streetAddress = elem.xpath(".//span[@class='addr']/a/text()")[0]
+
+    info = {
+        "type": "",
+        "land size": "",
+        "last sold": "",
+        "list": "",
+        "rent": "",
+        "bedrooms": "",
+        "bathrooms": "",
+        "sold": "",
+        "sold on": ""
+    }
+
+    for line in range(1,5):
+        lineSelector = elem.\
+            xpath(".//span[@class='addr']/../../..//table/tr[" + str(line) + "]/td")
+        info.update(translate(lineSelector))
+
     # Save found data
     scraperwiki.sqlite.save(unique_keys=['extracted_on','address'], data={
         "extracted_on": extractedOn,
-        "address": address,
-        "sold": sold.split()[1],
-        "sold_on": sold_on,
+        "address": streetAddress,
+        "sold": info["sold"],
+        "bathrooms": info["bathrooms"],
+        "land_size": info["land size"],
+        "type": info["type"],
+        "bedrooms": info["bedrooms"],
+        "list": info["list"],
+        "rent": info["rent"],
+        "land_size": info["land size"],
+        "suburb": suburb,
+        "sold_on": info["sold on"],
         "blob": blob
     })
+
 
 # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 
 # An arbitrary query against the database
-#print scraperwiki.sql.select("* from data")
+# print scraperwiki.sql.select("* from data")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
